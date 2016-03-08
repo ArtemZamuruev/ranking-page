b288dfea180bd84af102cff040e572ee7203e2ae
Artem Zamuruev <artem.zamuruev@gmail.com>
2016-03-08 10:44:39 +0000

    Правильное позиционирование сетки. Перерасчет сетки при изменении размеров окна

--- js/main.js
+++ js/main.js
@@ -1,35 +1,18 @@
-var valLengthPercent = 0.55;
-
-$(document).ready(function(){
-	
+$(document).ready(function(){	
 
 	for (var i = 0; i < rows.length; i++){
-
 		var rowHTML = '\
 			<div class="row '+rows[i]["category"]+'" order-number="'+(i+1)+'">\
 				<div class="row-description"><p>'+rows[i]["name"]+'</p></div>\
 				<div class="row-value" value="'+rows[i]["value"]+'"><p>'+rows[i]["value"]+'</p></div>\
 			</div>\
 		';
-
 		$("div.rows-stack").append(rowHTML);
-
 		var rowLengthInPercent = ((rows[i]["value"]*0.9*0.55)/0.4)*100;
-
 		$("div.row[order-number="+(i+1)+"] div.row-value").css({"width": rowLengthInPercent+"%"});
-
 	}
-
-
-	var rowsDivHeight = $("div.rows-stack").height();
-	//$("div.grid-background").css({"top"    : (rowsDivHeight+70)*(-1)});
-
-	// console.log("Body height before: "+$("body").height());
-
-	$("div.grid-background").css({"height" : rowsDivHeight+100});
-
-	// console.log("Body height after: "+$("body").height());
-
+	
+    backgroundGridSetup();
 });
 
 
@@ -43,3 +26,23 @@ $("div.button").hover(
 		$("div.row").css({"opacity" : "1"});
 	}
 );
+
+$(window).resize(function(){
+    backgroundGridSetup();
+});
+
+function prettifyValueNumbers(){
+    
+}
+
+function backgroundGridSetup(){
+    var rowsDivHeight = $("div.rows-stack").height();
+    
+    $("div.grid-background").css({"top": $("header").outerHeight()+30});
+	$("div.grid-background").css({"height" : rowsDivHeight+100});
+    
+    var scrWidth = $("div.grid-background").width();
+    
+    $("div.vertical-line").css({"margin-left": (scrWidth-74)/13+"px"});
+    $("div.vertical-line:nth-child(1)").css({"margin-left":"30px"});
+}
