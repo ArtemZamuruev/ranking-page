adc119addd7c7dad59454f0d87e2624c9de0146d
Artem Zamuruev <artem.zamuruev@gmail.com>
2016-03-09 19:40:28 +0000

    Доделаны описания к столбцам, изменены некоторые цвета, добавлены новые поля в описание колонок в файле rows.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
 $(document).ready(function(){	
-
-	for (var i = 0; i < rows.length; i++){
-		var rowHTML = '\
-			<div class="row '+rows[i]["category"]+'" order-number="'+(i+1)+'">\
-				<div class="row-description"><p>'+rows[i]["name"]+'</p></div>\
-				<div class="row-value" value="'+rows[i]["value"]+'"><p>'+rows[i]["value"]+'</p></div>\
-			</div>\
-		';
-		$("div.rows-stack").append(rowHTML);
-		var rowLengthInPercent = ((rows[i]["value"]*0.9*0.55)/0.4)*100;
-		$("div.row[order-number="+(i+1)+"] div.row-value").css({"width": rowLengthInPercent+"%"});
-	}
-	
+    displayRows();
     backgroundGridSetup();
+    prettifyValueNumbers();
 });
 
 
@@ -27,12 +16,104 @@ $("div.button").hover(
 	}
 );
 
+
 $(window).resize(function(){
     backgroundGridSetup();
 });
 
-function prettifyValueNumbers(){
+
+function displayRows(){
+    for (var i = 0; i < rows.length; i++){
+
+        var visibleValue;
+        if (rows[i]["val-number"]){
+            visibleValue = rows[i]["val-number"];
+        }
+        else{
+            visibleValue = rows[i]["value"];
+        }
+        
+        var descrHtml = "";
+       
+        if (rows[i]["des-type"] === "line"){
+            descrHtml = '<p>'+rows[i]["name"]+'</p>';
+        }
+        else{         
+            for (var d = 0; d < rows[i]["dots-num"]; d++){
+                descrHtml += '<div class = "dot"></div>';
+            }
+            
+            descrHtml += '\
+              <div class="description-circles">\
+                <div class="circle outer">\
+                    <div class="circle middle">\
+                        <div class="circle inner">\
+                            <p>'+rows[i]["name"]+'</p>\
+                        </div>\
+                    </div>\
+                </div>\
+              </div>\
+            ';
+        }
+
+        var rowHTML = '\
+            <div class="row '+rows[i]["category"]+'" order-number="'+(i+1)+'">\
+                <div class="row-description">'+descrHtml+'</p></div>\
+                <div class="row-value" value="'+rows[i]["value"]+'"><p>'+visibleValue+'</p></div>\
+            </div>\
+        ';
+        $("div.rows-stack").append(rowHTML);
+        
+        var rowLengthInPercent = ((rows[i]["value"]*0.9*0.55)/0.4)*100;
+        
+        $("div.row[order-number="+(i+1)+"] div.row-value").css({"width": rowLengthInPercent+"%"});
+        
+        if(rows[i]["des-type"] === "circle"){
+            setCirclesSizes($("div.row[order-number="+(i+1)+"]"),rows[i]["circle-rad"]); 
+            setDescrPSizes($("div.row[order-number="+(i+1)+"]"),rows[i]["font-sets"]);
+        }        
+    }
+}
+
+function setCirclesSizes(row, radius){
+    $("div.description-circles", row).css({
+        "top" : (radius - 13)*(-1)
+    });
     
+    $("div.circle.outer", row).css({
+        "width" : radius*2,
+        "height": radius*2
+    });
+    $("div.circle.middle", row).css({
+        "width" : radius*2-10,
+        "height": radius*2-10
+    });
+    $("div.circle.inner", row).css({
+        "width" : radius*2-20,
+        "height": radius*2-20
+    });
+}
+
+function setDescrPSizes(row, fontsets){
+    var params = fontsets.split(";");
+    $("div.row-description p", row).css({
+        "font-size" : params[0],
+        "top"       : params[1],
+        "left"      : params[2]
+    });
+}
+
+function prettifyValueNumbers(){
+    var valueRows = $("div.row-value");
+    for (var i = 0; i < valueRows.length; i++){
+        if ($("p", valueRows[i]).width() >= $(valueRows[i]).width()){
+            $("p", valueRows[i]).css({
+                "position" : "relative",
+                "left" : ($("p", valueRows[i]).width()+10)+"px",
+                "color": "#121212"
+            });
+        }
+    }
 }
 
 function backgroundGridSetup(){
--- js/rows.js
+++ js/rows.js
@@ -3,196 +3,318 @@ var rows = [
 		"name" 		: "Click-Through Rate",
 		"category" 	: "user-signals",
 		"value"		: "0.39",
-		"val-number": "0.67"
+		"val-number": "0.67",
+        "des-type"  : "circle",
+        "circle-rad": "70",
+        "dots-num"  : "28",
+        "font-sets" : "1.1em;38px;3px"
 	},
 	{
 		"name" 		: "Relevant terms",
 		"category" 	: "onpage-content",
-		"value"		: "0.34"
+		"value"		: "0.34",
+        "des-type"  : "circle",
+        "circle-rad": "50",
+        "dots-num"  : "6",
+        "font-sets" : "1em;20px;3px"
 	},
 	{
 		"name" 		: "Google +1",
 		"category" 	: "social",
-		"value"		: "0.33"
+		"value"		: "0.33",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Number of backlinks",
 		"category" 	: "backlinks",
-		"value"		: "0.31"
+		"value"		: "0.31",
+        "des-type"  : "circle",
+        "circle-rad": "65",
+        "dots-num"  : "32",
+        "font-sets" : "1.1em;30px;3px"
 	},
 	{
 		"name" 		: "Facebook total",
 		"category" 	: "social",
-		"value"		: "0.28"
+		"value"		: "0.28",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Pintrest",
 		"category" 	: "social",
-		"value"		: "0.27"
+		"value"		: "0.27",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "SEO visibility of backlinks URL",
 		"category" 	: "backlinks",
-		"value"		: "0.26"
+		"value"		: "0.26",
+        "des-type"  : "circle",
+        "circle-rad": "75",
+        "dots-num"  : "10",
+        "font-sets" : "1.1em;35px;3px"
 	},
 	{
 		"name" 		: "Tweets",
 		"category" 	: "social",
-		"value"		: "0.24"
+		"value"		: "0.24",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Refering domains",
 		"category" 	: "social",
-		"value"		: "0.23"
+		"value"		: "0.23",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "No-follow backlinks",
 		"category" 	: "backlinks",
-		"value"		: "0.23"
+		"value"		: "0.23",
+        "des-type"  : "circle",
+        "circle-rad": "55",
+        "dots-num"  : "15",
+        "font-sets" : "1.1em;20px;3px"
 	},
 	{
 		"name" 		: "Proof terms",
 		"category" 	: "onpage-content",
-		"value"		: "0.21"
+		"value"		: "0.21",
+        "des-type"  : "circle",
+        "circle-rad": "60",
+        "dots-num"  : "28",
+        "font-sets" : "1.1em;25px;3px"
 	},
 	{
 		"name" 		: "New backlinks",
 		"category" 	: "backlinks",
-		"value"		: "0.2"
+		"value"		: "0.2",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Backlinks from News Domains",
 		"category" 	: "backlinks",
-		"value"		: "0.2"
+		"value"		: "0.2",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "% Backlinks with keywords",
 		"category" 	: "backlinks",
-		"value"		: "0.17"
+		"value"		: "0.17",
+        "des-type"  : "circle",
+        "circle-rad": "50",
+        "dots-num"  : "15",
+        "font-sets" : "0.9em;3px;2px"
 	},
 	{
 		"name" 		: "% Backlinks with anchor = domain.com",
 		"category" 	: "backlinks",
-		"value"		: "0.16"
+		"value"		: "0.16",
+        "des-type"  : "circle",
+        "circle-rad": "55",
+        "dots-num"  : "25",
+        "font-sets" : "0.82em;20px;2px"
 	},
 	{
 		"name" 		: "Number of internal links",
 		"category" 	: "onpage-content",
-		"value"		: "0.16"
+		"value"		: "0.16",
+        "des-type"  : "circle",
+        "circle-rad": "75",
+        "dots-num"  : "34",
+        "font-sets" : "1.2em;40px;3px"
 	},
 	{
 		"name" 		: "% Backlinks with stopwords",
 		"category" 	: "backlinks",
-		"value"		: "0.16"
+		"value"		: "0.16",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Keywords in body",
 		"category" 	: "onpage-content",
-		"value"		: "0.15"
+		"value"		: "0.15",
+        "des-type"  : "circle",
+        "circle-rad": "50",
+        "dots-num"  : "30",
+        "font-sets" : "1em;20px;4px"
 	},
 	{
 		"name" 		: "HTML length",
 		"category" 	: "onpage-content",
-		"value"		: "0.14"
+		"value"		: "0.14",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Text character length",
 		"category" 	: "onpage-content",
-		"value"		: "0.13"
+		"value"		: "0.13",
+        "des-type"  : "circle",
+        "circle-rad": "55",
+        "dots-num"  : "25",
+        "font-sets" : "1.1em;10px;3px"
 	},
 	{
 		"name" 		: "Word count",
 		"category" 	: "onpage-content",
-		"value"		: "0.13"
+		"value"		: "0.13",
+        "des-type"  : "circle",
+        "circle-rad": "50",
+        "dots-num"  : "35",
+        "font-sets" : "1.2em;15px;3px"
 	},
 	{
 		"name" 		: "Sitespeed",
 		"category" 	: "onpage-technical",
-		"value"		: "0.11"
+		"value"		: "0.11",
+        "des-type"  : "circle",
+        "circle-rad": "45",
+        "dots-num"  : "8",
+        "font-sets" : "0.9em;22px;4px"
 	},
 	{
 		"name" 		: "Keywords in external links",
 		"category" 	: "onpage-content",
-		"value"		: "0.11"
+		"value"		: "0.11",
+        "des-type"  : "circle",
+        "circle-rad": "70",
+        "dots-num"  : "18",
+        "font-sets" : "1.3em;25px;3px"
 	},
 	{
 		"name" 		: "Word count in anchor text",
 		"category" 	: "backlinks",
-		"value"		: "0.11"
+		"value"		: "0.11",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Position of keywords in title",
 		"category" 	: "onpage-technical",
-		"value"		: "0.1"
+		"value"		: "0.1",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Length of URL",
 		"category" 	: "onpage-technical",
-		"value"		: "0.1"
+		"value"		: "0.1",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Keywords in internal links",
 		"category" 	: "onpage-content",
-		"value"		: "0.1"
+		"value"		: "0.1",
+        "des-type"  : "circle",
+        "circle-rad": "80",
+        "dots-num"  : "29",
+        "font-sets" : "1.5em;30px;3px"
 	},
 	{
 		"name" 		: "Time on site",
 		"category" 	: "user-signals",
-		"value"		: "0.09"
+		"value"		: "0.09",
+        "des-type"  : "circle",
+        "circle-rad": "50",
+        "dots-num"  : "20",
+        "font-sets" : "1.3em;11px;4px"
 	},
 	{
 		"name" 		: "Number of external links",
 		"category" 	: "onpage-content",
-		"value"		: "0.08"
+		"value"		: "0.08",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Backlinks from same country",
 		"category" 	: "backlinks",
-		"value"		: "0.07"
+		"value"		: "0.07",
+        "des-type"  : "circle",
+        "circle-rad": "65",
+        "dots-num"  : "16",
+        "font-sets" : "1.2em;20px;4px"
 	},
 	{
 		"name" 		: "H2",
 		"category" 	: "onpage-technical",
-		"value"		: "0.05"
+		"value"		: "0.05",
+        "des-type"  : "circle",
+        "circle-rad": "40",
+        "dots-num"  : "8",
+        "font-sets" : "1.5em;14px;-8px"
 	},
 	{
 		"name" 		: "Image count",
 		"category" 	: "onpage-content",
-		"value"		: "0.05"
+		"value"		: "0.05",
+        "des-type"  : "circle",
+        "circle-rad": "50",
+        "dots-num"  : "24",
+        "font-sets" : "1.3em;15px;3px"
 	},
 	{
 		"name" 		: "H1",
 		"category" 	: "onpage-technical",
-		"value"		: "0.05"
+		"value"		: "0.05",
+        "des-type"  : "circle",
+        "circle-rad": "55",
+        "dots-num"  : "33",
+        "font-sets" : "1.6em;25px;-20px"
 	},
 	{
 		"name" 		: "Description",
 		"category" 	: "onpage-technical",
-		"value"		: "0.05"
+		"value"		: "0.05",
+        "des-type"  : "circle",
+        "circle-rad": "45",
+        "dots-num"  : "25",
+        "font-sets" : "0.75em;21px;3px"
 	},
 	{
 		"name" 		: "Video integration",
 		"category" 	: "onpage-technical",
-		"value"		: "0.04"
+		"value"		: "0.04",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Bounce rate",
 		"category" 	: "user-signals",
-		"value"		: "0.04"
+		"value"		: "0.04",
+        "des-type"  : "circle",
+        "circle-rad": "50",
+        "dots-num"  : "21",
+        "font-sets" : "1.1em;16px;4px"
 	},
 	{
 		"name" 		: "Keyword in Title",
 		"category" 	: "onpage-technical",
-		"value"		: "0.02"
+		"value"		: "0.02",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Keyword in H2",
 		"category" 	: "onpage-content",
-		"value"		: "0.02"
+		"value"		: "0.02",
+        "des-type"  : "line",
+        "font-sets" : ""
 	},
 	{
 		"name" 		: "Keyword in Description",
 		"category" 	: "onpage-technical",
-		"value"		: "0.01"
+		"value"		: "0.01",
+        "des-type"  : "line",
+        "font-sets" : ""
 	}
 ];
